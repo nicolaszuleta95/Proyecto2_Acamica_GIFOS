996aac8ffc8d73e70d7996aa749c4bda45adddea
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-05-22 16:58:41 -0500

    Added gif username and titles

--- scripts/GetTrendingGifs.js
+++ scripts/GetTrendingGifs.js
@@ -1,25 +1,24 @@
 const apiKey = "hMezKtdsiOx17jANW8QDU3ipt0Kks8jK";
 const trendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=3&rating=g`;
 
-/*const gifSpace = document.querySelector(".gifSpace");*/
 let elements = document.getElementsByClassName("gifSpace");
+let titleGif = document.getElementsByClassName("tituloGIF");
+let userGif = document.getElementsByClassName("user");
 
 function renderGiphy(giphy) {
-  /* for (let i = 0; i < elements.length; i++) {
-    console.log(giphy);
-    const img = document.createElement("IMG");
-    img.src = giphy.images.downsized.url;
-    console.log(elements);
-    elements[i].appendChild(img);
-  }
-  console.log(elements); */
-
   for (let i = 0; i < elements.length; i++) {
     const img = document.createElement("IMG");
     img.src = giphy[i].images.downsized.url;
     elements[i].appendChild(img);
-    console.log(elements[i]);
-    console.log(img);
+  }
+}
+
+function fillGIF(giphy) {
+  for (let i = 0; i < titleGif.length; i++) {
+    titleGif[i].innerText = giphy[i].title;
+  }
+  for (let i = 0; i < userGif.length; i++) {
+    userGif[i].innerText = giphy[i].username;
   }
 }
 
@@ -27,4 +26,8 @@ fetch(trendingEndpoint)
   .then((res) => res.json())
   .then(({ data }) => {
     renderGiphy(data);
+    fillGIF(data);
+  })
+  .catch((error) => {
+    console.warn(`Error de ${error}`);
   });
