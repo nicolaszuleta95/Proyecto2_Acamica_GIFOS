b5a06ce2f4cc29f4f1e2ff7b0e0737e0d3c8bf4f
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-05-24 15:25:37 -0500

    developed the search bar wiht its functionalities, mobile version pending

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,16 @@
 const searchEndpoint = "https://api.giphy.com/v1/gifs/search";
+const suggEndpoint = "https://api.giphy.com/v1/tags/related/";
 
 let SEARCH_LIMIT = 10;
 let searchOffset = 0;
+let SUGGS_LIMIT = 4;
 let gifosCount = 0;
 
 const inputSearch = document.querySelector(".input-search");
 const btnSearch = document.querySelector(".searchButton");
+const btnSearchLeft = document.querySelector(".searchButtonLeft");
+const btnClose = document.querySelector(".XButton");
+const autocompleteList = document.querySelector(".autocomSearch");
 
 const requestSearch = () => {
   return new Promise((resolve, reject) => {
@@ -31,4 +36,53 @@ const handleToSearch = () => {
     });
 };
 
+const giveSuggs = async (searchText) => {
+  autocompleteList.classList.remove("dispnone");
+  inputSearch.classList.add("hideborder");
+  btnSearch.classList.add("hideX");
+  btnClose.classList.remove("hideX");
+  btnSearchLeft.classList.remove("hideX");
+
+  const res = await fetch(
+    `${suggEndpoint}${searchText}?api_key=${apiKey}&limit=${SUGGS_LIMIT}`
+  );
+  const suggs = await res.json();
+
+  if (searchText.length === 0) {
+    suggs.data = [];
+    autocompleteList.innerHTML = "";
+    autocompleteList.classList.add("dispnone");
+    inputSearch.classList.remove("hideborder");
+    btnSearch.classList.remove("hideX");
+    btnClose.classList.add("hideX");
+    btnSearchLeft.classList.add("hideX");
+  }
+
+  paintSuggsHTML(suggs.data);
+};
+
+const paintSuggsHTML = async (suggestions) => {
+  if (suggestions.length > 0) {
+    const html = suggestions
+      .map(
+        (match) =>
+          `<hr /><a href="#"><li><i class="fas fa-search"></i>${match.name}</li></a>`
+      )
+      .join("");
+    autocompleteList.innerHTML = html;
+  }
+};
+
+const closeSearch = () => {
+  inputSearch.value = "";
+  autocompleteList.innerHTML = "";
+  autocompleteList.classList.add("dispnone");
+  inputSearch.classList.remove("hideborder");
+  btnSearch.classList.remove("hideX");
+  btnClose.classList.add("hideX");
+};
+
 btnSearch.addEventListener("click", handleToSearch);
+btnSearchLeft.addEventListener("click", handleToSearch);
+inputSearch.addEventListener("input", () => giveSuggs(inputSearch.value));
+btnClose.addEventListener("click", () => closeSearch());
