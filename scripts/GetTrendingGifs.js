955a407de417c6fe43f37b251aa15e04554020e5
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-07-20 12:40:39 -0500

    tried to fix this shit with scss but its not working

--- scripts/GetTrendingGifs.js
+++ scripts/GetTrendingGifs.js
@@ -1,42 +1,7 @@
 import api from "./services/services.js";
 
-let elements = document.getElementsByClassName("gifSpace");
-let titleGif = document.getElementsByClassName("tituloGIF");
-let userGif = document.getElementsByClassName("user");
 let trendings = document.querySelector(".trendings");
 let trendGifsArr = [];
-
-// function renderGiphy(giphy) {
-//   for (let i = 0; i < elements.length; i++) {
-//     const img = document.createElement("IMG");
-//     img.src = giphy[i].images.original.url;
-//     img.id = giphy[i].id;
-//     img.classList.add("gif-img");
-//     elements[i].appendChild(img);
-//     elements[i].id = giphy[i].id;
-//     trendGifsArr.push(giphy[i]);
-//   }
-// }
-
-// function fillGIF(giphy) {
-//   for (let i = 0; i < titleGif.length; i++) {
-//     titleGif[i].innerText = giphy[i].title;
-//   }
-//   for (let i = 0; i < userGif.length; i++) {
-//     userGif[i].innerText = giphy[i].username;
-//   }
-// }
-
-// api
-//   .getTrending(3, 0)
-//   .then((res) => {
-//     const { data } = res;
-//     renderGiphy(data);
-//     fillGIF(data);
-//   })
-//   .catch((error) => console.warn("Error getTrending: ", error));
-
-//console.log(trendGifsArr);
 
 const paintTrendings = (data) => {
   const { id, title, username, images } = data;
@@ -73,11 +38,9 @@ const getTrendings = (arr) => {
   let gifs = "";
   for (let i = 0; i < arr.length; i++) {
     const gif = arr[i];
-    console.log(gif);
     gifs += paintTrendings(gif);
     trendings.innerHTML = gifs;
   }
 };
 
 readTrendings();
-console.log(trendGifsArr);
